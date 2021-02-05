260ee49e047649bae25142cffbea9960a557f5e4
Shaidurz <shaidurctg@gmail.com>
2021-02-05 08:01:50 +0600

    adding file

---
+++ callApply.js
@@ -0,0 +1,7 @@
+const normalPerson = {
+    firstName: 'Rahim',
+    lastName: 'Uddin',
+    getFullName: function(){
+        console.log(this.firstName, this.lastName)
+    }
+}
--- object-property.js
+++ object-property.js
@@ -18,10 +18,22 @@ const output = [];
 
 // const biggerId = students.filter( x => x.id <36);
 // console.log(biggerId);
-function doSomething(x, y = 4){
+// function doSomething(x, y = 4){
 
-    console.log(y);
+//     console.log(y);
     
-    }
+//     }
     
-    doSomething(3,2);
+//     doSomething(3,2);
+
+if( 'false' ){
+
+    console.log("I am not false")
+
+}
+
+else{
+
+    console.log("I am false")
+
+}
