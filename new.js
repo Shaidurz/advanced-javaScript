cebe1474c9b3845deec9f246c7c3773bc327a66b
Shaidurz <shaidurctg@gmail.com>
2021-02-06 06:24:42 +0600

    timeout, async, this, new added

---
+++ new.js
@@ -0,0 +1,10 @@
+class person {
+    constructor(firstName, lastName, salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        
+    }
+}
+const heroPerson = new person('Amatullah', 'Wazeeha', 25000);
+console.log(heroPerson);
---
+++ timeout.js
@@ -0,0 +1,21 @@
+// function dublin (){
+//     console.log("Fulki");
+// }
+// dublin()
+// console.log(4444);
+// console.log(3333);
+// setTimeout (dublin, 3000)
+// console.log(4444);
+// console.log(6666);
+
+function dublin (){
+    console.log("Fulki");
+}
+dublin()
+console.log(4444);
+console.log(3333);
+setInterval(() => {
+    console.log("muri kha")
+}, 3000);
+console.log(4444);
+console.log(6666);
